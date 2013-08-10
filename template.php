7a92f7c5c7346f940d2a285516ce74abee01b780
Jim Kirkpatrick <git@i-jk.co.uk>
2013-08-10 19:38:33 +0100

    Remove Radix dropdown

--- template.php
+++ template.php
@@ -10,3 +10,15 @@ require_once $theme_path . '/includes/form.inc';
 require_once $theme_path . '/includes/menu.inc';
 require_once $theme_path . '/includes/comment.inc';
 require_once $theme_path . '/includes/node.inc';
+
+
+/**
+ * Implements hook_css_alter().
+ */
+function wincc_theme_js_alter(&$js) {
+  // Unset some panopoly css.
+  $radix_path = drupal_get_path('theme', 'radix');
+  if (isset($js[$radix_path . '/assets/javascripts/radix-script.js'])) {
+    unset($js[$radix_path . '/assets/javascripts/radix-script.js']);
+  }
+}
