2d90c5c2dde81cab34aabbc6c74c4fc10f52d48b
Jim Kirkpatrick <git@i-jk.co.uk>
2013-08-17 15:44:44 +0100

    Grass for footer colouring

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -10,111 +10,112 @@
  * @see html.tpl.php
  */
 ?>
-<header id="header" class="header" role="header">
-  <div class="container clearfix">
-    <?php if ($logo || $site_name): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="pull-left brand">
-        <?php if ($logo): ?>
-          <img id="logo" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        <?php endif; ?>
-        <?php if ($site_name): ?>
-          <h1 id="site-name" class="element-invisible"><?php print $site_name; ?></h1>
-        <?php endif; ?>
-      </a>
-    <?php endif; ?>
-    <nav class='navbar'>
-      <div class="navbar-inner">
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <a class="btn btn-navbar btn-navbar-menu" data-toggle="collapse" data-target=".nav-menu-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </a>
-        <!-- .btn-navbar-search for collapsed search form -->
-        <?php if ($search_form): ?>
-          <a class="btn btn-navbar btn-navbar-search" data-toggle="collapse" data-target=".nav-search-collapse">
-            <span class="icon-search"></span>
-          </a>
-        <?php endif; ?>
-
-        <div class="nav-collapse nav-menu-collapse">
-          <div class="inner">
-            <?php if ($main_menu_expanded): ?>
-              <?php print theme('links__system_main_menu', array(
-                'links' => $main_menu_expanded,
-                'attributes' => array(
-                  'class' => array('main-menu nav pull-left'),
-                  'id' => array('main-menu'),
-                  'role' => array('navigation'),
-                ),
-                'heading' => array(
-                  'text' => t('Main menu'),
-                  'level' => 'h2',
-                  'class' => array('element-invisible'),
-                ),
-              )); ?>
+<div id="primary-page">
+  <header id="header" class="header" role="header">
+    <div class="container clearfix">
+      <?php if ($logo || $site_name): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="pull-left brand">
+          <?php if ($logo): ?>
+            <img id="logo" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           <?php endif; ?>
-              <!--nav id="main-menu" class="main-menu pull-left" role="navigation">
-                <?php //print render($main_menu); ?>
-              </nav--> <!-- /#main-menu -->
-          </div>
-        </div>
+          <?php if ($site_name): ?>
+            <h1 id="site-name" class="element-invisible"><?php print $site_name; ?></h1>
+          <?php endif; ?>
+        </a>
+      <?php endif; ?>
+      <nav class='navbar'>
+        <div class="navbar-inner">
+          <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+          <a class="btn btn-navbar btn-navbar-menu" data-toggle="collapse" data-target=".nav-menu-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <!-- .btn-navbar-search for collapsed search form -->
+          <?php if ($search_form): ?>
+            <a class="btn btn-navbar btn-navbar-search" data-toggle="collapse" data-target=".nav-search-collapse">
+              <span class="icon-search"></span>
+            </a>
+          <?php endif; ?>
 
-        <div class="nav-collapse nav-search-collapse">
-          <div class="inner">
-            <?php if ($search_form): ?>
-              <?php print $search_form; ?>
+          <div class="nav-collapse nav-menu-collapse">
+            <div class="inner">
+              <?php if ($main_menu_expanded): ?>
+                <?php print theme('links__system_main_menu', array(
+                  'links' => $main_menu_expanded,
+                  'attributes' => array(
+                    'class' => array('main-menu nav pull-left'),
+                    'id' => array('main-menu'),
+                    'role' => array('navigation'),
+                  ),
+                  'heading' => array(
+                    'text' => t('Main menu'),
+                    'level' => 'h2',
+                    'class' => array('element-invisible'),
+                  ),
+                )); ?>
             <?php endif; ?>
+                <!--nav id="main-menu" class="main-menu pull-left" role="navigation">
+                  <?php //print render($main_menu); ?>
+                </nav--> <!-- /#main-menu -->
+            </div>
           </div>
-        </div>
 
-      </div>
-    </nav> <!-- /#navigation -->
-  </div>
-</header>
-
-<div id="main-wrapper">
-  <div id="main" class="main container">
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb" class="visible-desktop">
-        <div class="container">
-          <?php print $breadcrumb; ?>
-        </div>
-      </div>
-    <?php endif; ?>
-    <?php if ($messages): ?>
-      <div id="messages">
-        <div class="container">
-          <?php print $messages; ?>
-        </div>
-      </div>
-    <?php endif; ?>
-    <div id="content">
-      <a id="main-content"></a>
-      <div id="page-header">
-          <div class="container">
-            <?php if ($title): ?>
-              <div class="page-header">
-                <h1 class="title"><?php print $title; ?></h1>
-              </div>
-            <?php endif; ?>
-            <?php if ($tabs): ?>
-              <div class="tabs">
-                <?php print render($tabs); ?>
-              </div>
-            <?php endif; ?>
-            <?php if ($action_links): ?>
-              <ul class="nav nav-pills action-links">
-                <?php print render($action_links); ?>
-              </ul>
-            <?php endif; ?>
+          <div class="nav-collapse nav-search-collapse">
+            <div class="inner">
+              <?php if ($search_form): ?>
+                <?php print $search_form; ?>
+              <?php endif; ?>
+            </div>
           </div>
-      </div>
-      <?php print render($page['content']); ?>
+
+        </div>
+      </nav> <!-- /#navigation -->
     </div>
-  </div>
-</div> <!-- /#main-wrapper -->
+  </header>
 
+  <div id="main-wrapper">
+    <div id="main" class="main container">
+      <?php if ($breadcrumb): ?>
+        <div id="breadcrumb" class="visible-desktop">
+          <div class="container">
+            <?php print $breadcrumb; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+      <?php if ($messages): ?>
+        <div id="messages">
+          <div class="container">
+            <?php print $messages; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+      <div id="content">
+        <a id="main-content"></a>
+        <div id="page-header">
+            <div class="container">
+              <?php if ($title): ?>
+                <div class="page-header">
+                  <h1 class="title"><?php print $title; ?></h1>
+                </div>
+              <?php endif; ?>
+              <?php if ($tabs): ?>
+                <div class="tabs">
+                  <?php print render($tabs); ?>
+                </div>
+              <?php endif; ?>
+              <?php if ($action_links): ?>
+                <ul class="nav nav-pills action-links">
+                  <?php print render($action_links); ?>
+                </ul>
+              <?php endif; ?>
+            </div>
+        </div>
+        <?php print render($page['content']); ?>
+      </div>
+    </div>
+  </div> <!-- /#main-wrapper -->
+</div>
 <footer id="footer" class="footer" role="footer">
   <div class="container">
     <?php if ($copyright): ?>
